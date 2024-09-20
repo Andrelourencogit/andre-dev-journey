cd98df7d8d42b42aea4c4be759c118764b19f94a
André Lourenço <andrelourenco@info.ufrn.br>
2024-09-20 07:46:38 -0300

    Corrige responsividade

--- src/components/structure/Footer/Footer.tsx
+++ src/components/structure/Footer/Footer.tsx
@@ -2,12 +2,16 @@ import './Footer.scss';
 import emailLogo from '../../../assets/icons/email.svg';
 import linkedinLogo from '../../../assets/icons/linkedin.svg';
 import githubLogo from '../../../assets/icons/github.svg';
+import whatsappLogo from '../../../assets/icons/whatsapp.svg';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; 2024, por Andre Lourenço.</p>
       <div className="footer-icons">
+        <a href="https://api.whatsapp.com/send/?phone=5584991573728&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">
+          <img src={whatsappLogo} alt="whatsapp" />
+        </a>
         <a href="mailto:lourencoandre80@gmail.com" target="_blank" rel="noopener noreferrer">
           <img src={emailLogo} alt="Email" />
         </a>
